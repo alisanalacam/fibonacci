19070c35a81d09f011e63100be19647c9c5245d1
Alişan ALAÇAM <alisanalacam@gmail.com>
2016-03-29 08:47:18 +0300

    girilen sayının fibonacci hesaplaması yapıldı.

--- app/Http/Controllers/FibonacciController.php
+++ app/Http/Controllers/FibonacciController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class FibonacciController extends Controller
@@ -9,7 +10,33 @@ class FibonacciController extends Controller
 
     public function getListOfFibonacci(Request $request)
     {
-        return view('fibonacci');
+        $lists = [];
+
+        if ($request->isMethod('POST')) {
+            $validator = Validator::make($request->all(), [
+                'count' => 'required|min:1|max:100'
+            ]);
+
+            if ($validator->fails()) {
+                return redirect('/')
+                    ->withErrors($validator)
+                    ->withInput();
+            }
+
+            $lists = $this->getFibonacciByCount($request->get('count'));
+        }
+
+        return view('fibonacci', compact('lists'));
+    }
+
+    public function getFibonacciByCount($count, $first = 0, $second = 1)
+    {
+        $fib = [$first, $second];
+        for($i = 1; $i < $count-1; $i++)
+        {
+            $fib[] = $fib[$i] + $fib[$i-1];
+        }
+        return $fib;
     }
     
 }
